05b5dd8e5fa3eaf140d8f9d180afe8b082624761
binhth <binhth@hocmai.vn>
2024-03-05 16:50:12 +0700

    feat: flutter day 11

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -7,9 +7,11 @@ class ExpensesList extends StatelessWidget {
   const ExpensesList({
     super.key,
     required this.expenses,
+    required this.onRemoveExpense,
   });
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +19,13 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       // scrollDirection: Axis.vertical,
       // shrinkWrap: true,
-      itemBuilder: (ctx, index) => ExpensesItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpensesItem(expenses[index]),
+      ),
     );
   }
 }
